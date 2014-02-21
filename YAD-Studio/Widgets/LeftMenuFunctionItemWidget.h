efba64046d97bfbaa7c10f507bf431555d9b0e4a
Yura <yuragri@gmail.com>
2014-02-21 17:12:25 +0200

    #5 created events for mouse click

--- YAD-Studio/Widgets/LeftMenuFunctionItemWidget.h
+++ YAD-Studio/Widgets/LeftMenuFunctionItemWidget.h
@@ -6,17 +6,30 @@
 namespace Ui {
 class LeftMenuFunctionItemWidget;
 }
-
+#include <QListWidgetItem>
 class LeftMenuFunctionItemWidget : public QWidget
 {
     Q_OBJECT
-    
+signals:
+    // define mouseClick signal
+    void onFocus();
 public:
+    void setName(const QString name);
+    void setComment(const QString comment);
     explicit LeftMenuFunctionItemWidget(QWidget *parent = 0);
     ~LeftMenuFunctionItemWidget();
-    
+
+protected:
+    // re-implement processing of mouse events
+    void mouseReleaseEvent ( QMouseEvent * e );
+    void mousePressEvent ( QMouseEvent * e );
+
 private:
     Ui::LeftMenuFunctionItemWidget *ui;
+    // member variable to store click position
+    QPoint _m_lastPoint;
+    // member variable - flag of click beginning
+    bool _m_mouseClick;
 };
 
 #endif // LEFTMENUFUNCTIONITEMWIDGET_H
