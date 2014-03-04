5bad8817534e876d36c1319ca6dbcd1af19574ae
Diana Sirenko <dianasirenko@gmail.com>
2014-03-04 17:45:04 +0200

    close #16 create Compiler Error class

--- YAD-Studio/Logic/CompilerError.cpp
+++ YAD-Studio/Logic/CompilerError.cpp
@@ -1,5 +1,56 @@
 #include "CompilerError.h"
 
-CompilerError::CompilerError()
+CompilerError::CompilerError(const QString &title,
+                             const QString &description,
+                             int error_number,
+                             int line_numer):
+    _title(title),
+    _description(description),
+    _error_number(error_number),
+    _line_numer(line_numer)
 {
+}
+int CompilerError::getErrorNumner()
+{
+    return _error_number;
+}
+
+int CompilerError::getLineNumber()
+{
+    return _line_numer;
+}
+
+const QString& CompilerError::getErrorTitle()
+{
+    return _title;
+}
+
+const QString& CompilerError::getErrorDescription()
+{
+    return _description;
+}
+
+const QString CompilerError::getFullErrorInfo()
+{
+    return _title + ": " + _description;
+}
+
+void CompilerError::setErrorNumber(int number)
+{
+    _error_number = number;
+}
+
+void CompilerError::setLineNumber(int line)
+{
+    _line_numer = line;
+}
+
+void CompilerError::setErrorTitle(const QString& title)
+{
+    _title = title;
+}
+
+void CompilerError::setErrorDescription(const QString& description)
+{
+    _description = description;
 }
--- YAD-Studio/Logic/CompilerError.h
+++ YAD-Studio/Logic/CompilerError.h
@@ -1,10 +1,39 @@
 #ifndef COMPILERERROR_H
 #define COMPILERERROR_H
+#include "QString"
 
 class CompilerError
 {
 public:
-    CompilerError();
+    CompilerError( const QString& title,
+                   const QString& description,
+                   int error_number,
+                   int line_numer=-1);
+
+    int getErrorNumner();
+    int getLineNumber();
+    const QString& getErrorTitle();
+    const QString& getErrorDescription();
+    /**
+     * @brief getFullErrorInfo
+     * @return ErrorTitle + ErrorDescription
+     */
+    const QString getFullErrorInfo();
+
+    void setErrorNumber(int number);
+    void setLineNumber(int line);
+    void setErrorTitle(const QString& title);
+    void setErrorDescription(const QString& description);
+
+private:
+    // each error type has own number
+    int _error_number;
+
+    //number of line, where the error was occurred
+    int _line_numer;
+
+    QString _title;
+    QString _description;
 };
 
 #endif // COMPILERERROR_H
