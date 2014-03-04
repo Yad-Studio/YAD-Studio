53f6ebd6f2a6244fd950769063ec8285fbe9b25b
Diana Sirenko <dianasirenko@gmail.com>
2014-03-04 17:29:25 +0200

    close #15 add RunError

--- YAD-Studio/Logic/RunError.cpp
+++ YAD-Studio/Logic/RunError.cpp
@@ -1,5 +1,43 @@
 #include "RunError.h"
 
-RunError::RunError()
+RunError::RunError(const QString& title,
+                   const QString& description,
+                   int error_numer):
+    _title(title),
+    _description(description),
+    _error_number(error_numer)
 {
+}
+int RunError::getErrorNumner()
+{
+    return _error_number;
+}
+
+const QString& RunError::getErrorTitle()
+{
+    return _title;
+}
+
+const QString& RunError::getErrorDescription()
+{
+    return _description;
+}
+
+const QString RunError::getFullErrorInfo()
+{
+    return _title + ": " + _description;
+}
+
+void RunError::setErrorNumber(int number)
+{
+    _error_number = number;
+}
+void RunError::setErrorTitle(const QString& title)
+{
+    _title = title;
+}
+
+void RunError::setErrorDescription(const QString& description)
+{
+    _description = description;
 }
--- YAD-Studio/Logic/RunError.h
+++ YAD-Studio/Logic/RunError.h
@@ -1,10 +1,31 @@
 #ifndef RUNERROR_H
 #define RUNERROR_H
+#include "QString"
 
 class RunError
 {
 public:
-    RunError();
+    RunError(const QString& title, const QString& description,
+             int error_numer);
+
+    int getErrorNumner();
+    const QString& getErrorTitle();
+    const QString& getErrorDescription();
+    /**
+     * @brief getFullErrorInfo
+     * @return ErrorTitle + ErrorDescription
+     */
+    const QString getFullErrorInfo();
+
+    void setErrorNumber(int number);
+    void setErrorTitle(const QString& title);
+    void setErrorDescription(const QString& description);
+private:
+    // each error type has own number
+    int _error_number;
+
+    QString _title;
+    QString _description;
 };
 
 #endif // RUNERROR_H
