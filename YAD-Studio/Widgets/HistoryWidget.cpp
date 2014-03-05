e6dae7ccb94169544ae22c27a1b913e01e773f86
Diana Sirenko <dianasirenko@gmail.com>
2014-03-05 17:17:56 +0200

    #20

--- YAD-Studio/Widgets/HistoryWidget.cpp
+++ YAD-Studio/Widgets/HistoryWidget.cpp
@@ -1,14 +1,32 @@
 #include "HistoryWidget.h"
 #include "ui_HistoryWidget.h"
+#include <QStringListModel>
 
 HistoryWidget::HistoryWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::HistoryWidget)
 {
     ui->setupUi(this);
+
+    QVector<QString> vec;
+    vec.push_back("test1");
+    vec.push_back("test2");
+    historyChanged(vec);
 }
 
 HistoryWidget::~HistoryWidget()
 {
     delete ui;
+}
+void HistoryWidget::historyChanged(QVector<QString> list)
+{
+    QStringList* stringList = new QStringList();
+
+    for(int i=0; i<list.size(); ++i)
+    {
+        stringList->append(list[i]);
+    }
+
+    QStringListModel* listModel = new QStringListModel(*stringList, NULL);
+    ui->listView->setModel(listModel);
 }
--- YAD-Studio/Widgets/HistoryWidget.h
+++ YAD-Studio/Widgets/HistoryWidget.h
@@ -17,6 +17,21 @@ public:
     
 private:
     Ui::HistoryWidget *ui;
+
+public slots:
+    // the list in widget must be updated
+    //(delete all previous history and write new QVector)
+    void historyChanged(QVector<QString> list);
+
+signals:
+    //signal is triggered when user double clicks the word in the list.
+    //The parameter is the word which user clicked.
+    void inputWordSelected(QString word);
+
+    //signal should be emitted when user deletes the word from the list.
+    //There is no need to delete word manually from the list
+    //as historyChanged will be called automatically.
+    void removeFromHistory(QString word);
 };
 
 #endif // HISTORYWIDGET_H
