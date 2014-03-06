5e94c261b3e078622697677e4655da22ba201c2f
Andriy Chaika <andriy@chaika.in.ua>
2014-03-06 15:45:22 +0200

    #13 YadFile basic implementation

--- YAD-Studio/Logic/YadFile.cpp
+++ YAD-Studio/Logic/YadFile.cpp
@@ -1,5 +1,39 @@
 #include "YadFile.h"
-
-YadFile::YadFile()
+#include <QDebug>
+YadFile::YadFile(
+        QString source_code,
+        QVector<QString> history)
+    : _source_code(source_code),
+      _history(history)
 {
+
+}
+
+QString YadFile::getSourceCode() const
+{
+    return _source_code;
+}
+
+
+QVector<QString> YadFile::getHistory() const
+{
+    return _history;
+}
+
+void YadFile::saveToFile(QString file_name) const
+{
+    qDebug() << "Saved to file: " << file_name;
+}
+
+
+YadFile YadFile::readFromFile(QString file_name)
+{
+    qDebug() << "Read from file: " << file_name;
+
+    QString source = "//Comment\nT = {a,b, c}\n aa->b \n bb->a\n\n";
+    QVector<QString> history;
+    history.push_back("aaaaabbbb");
+    history.push_back("aaabb");
+
+    return YadFile(source, history);
 }
--- YAD-Studio/Logic/YadFile.h
+++ YAD-Studio/Logic/YadFile.h
@@ -1,10 +1,62 @@
 #ifndef YADFILE_H
 #define YADFILE_H
+#include <QVector>
+#include <QString>
 
+/**
+ * @brief Saves and loads data from file
+ */
 class YadFile
 {
 public:
-    YadFile();
+    /**
+     * @brief YadFile
+     * @param source_code complete source code (with comments)
+     * @param history history of launches
+     */
+    YadFile(QString source_code,
+            QVector<QString> history);
+
+    /**
+     * @brief Returns source code
+     * @return
+     */
+    QString getSourceCode() const;
+
+    /**
+     * @brief Returns history of launches
+     * @return
+     */
+    QVector<QString> getHistory() const;
+
+    /**
+     * @brief  method saves to file source code and history to a given file.
+     * When any error occurs then
+     * the method should throw QException with error description
+     * @param file_name
+     */
+    void saveToFile(QString file_name) const;
+
+    /**
+     * @brief  method reads source code and history from file.
+     * When any error occurs then the method should throw
+     * QException with error description
+     * @param file_name
+     * @return
+     */
+    static YadFile readFromFile(QString file_name);
+private:
+
+
+    /**
+     * @brief complete source code (with comments)
+     */
+    QString _source_code;
+    /**
+     * @brief history of launches
+     */
+    QVector<QString> _history;
+
 };
 
 #endif // YADFILE_H
