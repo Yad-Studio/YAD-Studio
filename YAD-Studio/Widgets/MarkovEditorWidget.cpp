5bda45fe3332bdfec6cd334c4be67b98478f7510
Andriy Chaika <andriy@chaika.in.ua>
2014-03-06 16:23:18 +0200

    close #23 MarkovEditorWidget essential features

--- YAD-Studio/Widgets/MarkovEditorWidget.cpp
+++ YAD-Studio/Widgets/MarkovEditorWidget.cpp
@@ -1,11 +1,83 @@
 #include "MarkovEditorWidget.h"
+#include <QDebug>
+#include <QFont>
+#include <QFontMetrics>
+#include "Logic/MarkovParser.h"
 
 MarkovEditorWidget::MarkovEditorWidget(QWidget *parent) :
-    QTextEdit(parent)
+    QPlainTextEdit(parent),
+    _can_run(false)
 {
+    connect(this, SIGNAL(textChanged()), this, SLOT(userChangedSourceCode()));
 
+    //Set font
+    QFont font;
+    font.setFamily("Courier");
+    font.setStyleHint(QFont::Monospace);
+    font.setFixedPitch(true);
+    font.setPointSize(10);
+
+    this->setFont(font);
+
+    //Set tab width
+    QFontMetrics metrics(font);
+    this->setTabStopWidth(4 * metrics.width(' '));
+
+
+    this->newSourceCode("T = {a,b,c}\na->b");
 }
 
 MarkovEditorWidget::~MarkovEditorWidget()
 {
+}
+
+
+void MarkovEditorWidget::newSourceCode(QString source_code)
+{
+    this->setPlainText(source_code);
+}
+
+void MarkovEditorWidget::deleteSelection()
+{
+    QTextCursor cursor = this->textCursor();
+    cursor.removeSelectedText();
+    this->setTextCursor(cursor);
+}
+void MarkovEditorWidget::setCanRun(bool new_value)
+{
+    if(new_value != _can_run)
+    {
+        _can_run = new_value;
+        emit canRun(_can_run);
+    }
+}
+
+void MarkovEditorWidget::userChangedSourceCode()
+{
+    QString source_code = this->toPlainText();
+    emit sourceCodeChanged(source_code);
+
+    bool res = MarkovParser::parseSourceCode(source_code, _algorithm, _errors);
+
+    if(res)
+    {
+        emit markovAlgorithmChanged(_algorithm);
+        setCanRun(true);
+    }
+    else
+    {
+        setCanRun(false);
+        updateErrors();
+    }
+}
+
+void MarkovEditorWidget::updateErrors()
+{
+    for(int i=0; i<_errors.size(); ++i)
+    {
+        CompilerError err = _errors[i];
+        qDebug() << "Error: " << err.getErrorNumber() << " line: " << err.getLineNumber() <<
+                    "Title: " << err.getErrorTitle() << err.getErrorDescription();
+
+    }
 }
--- YAD-Studio/Widgets/MarkovEditorWidget.h
+++ YAD-Studio/Widgets/MarkovEditorWidget.h
@@ -1,19 +1,75 @@
 #ifndef MARKOVEDITORWIDGET_H
 #define MARKOVEDITORWIDGET_H
 
-#include <QTextEdit>
+#include <QPlainTextEdit>
+#include "Logic/CompilerError.h"
+#include "Logic/MarkovAlgorithm.h"
 
-
-class MarkovEditorWidget : public QTextEdit
+class MarkovEditorWidget : public QPlainTextEdit
 {
     Q_OBJECT
     
 public:
     explicit MarkovEditorWidget(QWidget *parent = 0);
     ~MarkovEditorWidget();
-    
+public slots:
+
+    /**
+     * @brief when source code from file is loaded this slot will be called.
+     * Source code should be put to QTextEditor.
+     * @param source_code
+     */
+    void newSourceCode(QString source_code);
+
+    /**
+     * @brief the text which is selected should be deleted.
+     */
+    void deleteSelection();
+
+signals:
+    /**
+     * @brief if any changes where done to source code
+     * @param source_code
+     */
+    void sourceCodeChanged(QString source_code);
+
+    /**
+     * @brief if algorithm was updated
+     */
+    void markovAlgorithmChanged(MarkovAlgorithm algorithm);
+
+    /**
+     * @brief  when _can_run changes.
+     */
+    void canRun(bool);
 private:
 
+
+    void setCanRun(bool new_value);
+
+    /**
+     * @brief list of errors which are in source code
+     */
+    QVector<CompilerError> _errors;
+    /**
+     * @brief last successfully parsed algorithm
+     */
+    MarkovAlgorithm _algorithm;
+
+    /**
+     * @brief list of active break points
+     */
+    QVector<int> _breakpoints;
+
+    /**
+     * @brief if the are no errors it should be true and false otherwise.
+     * When changed canRun should be emitted.
+     * MUST BE CHANGED ONLY BY setCanRun(bool)
+     */
+    bool _can_run;
+private slots:
+    void userChangedSourceCode();
+    void updateErrors();
 };
 
 #endif // MARKOVEDITORWIDGET_H
