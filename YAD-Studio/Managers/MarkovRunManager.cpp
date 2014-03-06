27e8e58c9a592005964f18ed2ee2bc6210e7f58e
Diana Sirenko <dianasirenko@gmail.com>
2014-03-06 17:40:30 +0200

    #29

--- YAD-Studio/Managers/MarkovRunManager.cpp
+++ YAD-Studio/Managers/MarkovRunManager.cpp
@@ -1,5 +1,216 @@
 #include "MarkovRunManager.h"
 
-MarkovRunManager::MarkovRunManager()
+
+MarkovRunManager* MarkovRunManager::_instance = nullptr;
+
+MarkovRunManager* MarkovRunManager::getInstance( )
 {
+      if (_instance == nullptr)
+        _instance = new MarkovRunManager();
+     return _instance;
+}
+
+MarkovRunManager::MarkovRunManager():
+        _input_word(""),
+        _steps_made(0),
+        _word_after_last_step(""),
+        _is_debug_mode(false)
+{
+}
+int MarkovRunManager::getStepNumberOfValue(QString word)
+{
+    //TODO: implement
+    return 2;
+}
+bool MarkovRunManager::choseAndUseRule(QString& word,
+                                       MarkovRule& rule)
+{
+    //TODO: implement
+    return true;
+}
+
+bool MarkovRunManager::findAndApplyNextRule()
+{
+    if (_steps_history.contains(StepResult(_word_after_last_step,0)))
+    {
+        if(_is_debug_mode)
+        {
+            int prev_same_stem = getStepNumberOfValue(_input_word);
+
+            QString description = "The result of step#"+prev_same_stem;
+            description+=" is same as on the step #";
+            description+=_steps_made;
+
+            RunError error("Algorithm never terminates",
+                           description,
+                           102);
+            emit debugFinishFail(_input_word,error,_steps_made);
+        }
+        else
+        {
+            int prev_same_stem = getStepNumberOfValue(_input_word);
+
+            QString description = "The result of step#"+prev_same_stem;
+            description+=" is same as on the step #";
+            description+=_steps_made;
+
+            RunError error("Algorithm never terminates",
+                           description,
+                           102);
+            emit runWithoutDebugFinishFail(_input_word,error,_steps_made);
+        }
+
+        return false;
+    }
+
+    if(_word_after_last_step.size()>2000)
+    {
+        if(_is_debug_mode)
+        {
+            QString description = "Result can not be longer than 2000 symbols. On step #"+_steps_made;
+            description+=" input become ";
+            description+=_word_after_last_step.size();
+
+            RunError error("Too long result",
+                           description,
+                           101);
+            emit debugFinishFail(_input_word,error,_steps_made);
+        }
+        else
+        {
+            QString description = "Result can not be longer than 2000 symbols. On step #"+_steps_made;
+            description+=" input become ";
+            description+=_word_after_last_step.size();
+
+            RunError error("Too long result",
+                           description,
+                           101);
+            emit runWithoutDebugFinishFail(_input_word,error,_steps_made);
+        }
+        return false;
+    }
+
+//Go through MarkovRules and select which fits.
+//If there are no rules then emit debugFinishSuccess or
+//runWithoutDebugFinishSuccess depending on run mode.
+//Use as _word_after_last_step as output word.
+
+    QString word = _word_after_last_step;
+    MarkovRule rule;
+    if(!choseAndUseRule(word, rule))
+    {
+        //there are no rules
+        if(_is_debug_mode)
+        {
+            emit debugFinishSuccess(_input_word,
+                                    word,
+                                    _steps_made);
+        }
+        else
+        {
+            emit runWithoutDebugFinishSuccess(_input_word,
+                                              word,
+                                              _steps_made);
+        }
+    }
+
+    ++_steps_made;
+
+    if(_is_debug_mode)
+    {
+        emit debugStepFinished(_steps_made,
+                               _word_after_last_step,
+                               word,
+                               rule);
+    }
+
+    _word_after_last_step = word;
+
+    _steps_history.insert(StepResult(_word_after_last_step,_steps_made));
+
+
+    if(_is_debug_mode)
+    {
+        if(_break_points.contains(rule.getLineNumber()))
+        {
+            emit debugBreakPointReached(rule.getLineNumber());
+            return false;
+        }
+    }
+
+//If rule is final then emit debugFinishSuccess or
+//runWithoutDebugFinishSuccess depending on run mode.
+//Use as _word_after_last_step as output word. return false.
+
+    if(rule.isFinalRule())
+    {
+        if(_is_debug_mode)
+        {
+            emit debugFinishSuccess(_input_word,
+                                    _word_after_last_step,
+                                    _steps_made);
+        }
+        else
+        {
+            emit runWithoutDebugFinishSuccess(_input_word,
+                                              _word_after_last_step,
+                                              _steps_made);
+        }
+        return false;
+    }
+
+    return true;
+}
+
+void MarkovRunManager::setAlgorithm(MarkovAlgorithm algorithm)
+{
+    _algorithm = algorithm;
+}
+
+void MarkovRunManager::setCanRunSourceCode(bool can)
+{
+    emit canRunSourceCode(can);
+}
+
+void MarkovRunManager::runWithoutDebug(QString input_word)
+{
+
+}
+
+void MarkovRunManager::runWithDebug(QString input_word)
+{
+
+}
+
+void MarkovRunManager::addBreakPoint(int line_number)
+{
+
+}
+
+void MarkovRunManager::removeBreakPoint(int line_number)
+{
+
+}
+
+void MarkovRunManager::debugNextStep()
+{
+
+}
+
+void MarkovRunManager::debugContinue()
+{
+
+}
+
+void MarkovRunManager::debugStop()
+{
+
+}
+bool operator<(const StepResult& a, const StepResult& b)
+{
+   return a._output < b._output;
+}
+bool operator==(const StepResult& a, const StepResult& b)
+{
+    return a._output == b._output;
 }
--- YAD-Studio/Managers/MarkovRunManager.h
+++ YAD-Studio/Managers/MarkovRunManager.h
@@ -1,10 +1,139 @@
 #ifndef MARKOVRUNMANAGER_H
 #define MARKOVRUNMANAGER_H
+#include <QObject>
+#include "Logic/MarkovAlgorithm.h"
+#include "Logic/RunError.h"
+#include "QSet"
+#include "QHash"
 
-class MarkovRunManager
+
+class StepResult
 {
 public:
-    MarkovRunManager();
+    StepResult(QString output,
+               int step_id):
+        _output(output),
+        _step_id(step_id)
+    {
+    }
+
+    QString _output; //The word after rule on this step was applied
+    int _step_id; //The id of step
 };
+bool operator <(const StepResult& a, const StepResult& b);
+bool operator==(const StepResult& a, const StepResult& b);
+
+inline uint qHash(const StepResult& a)
+{
+     return qHash(a._output);
+}
+
+class MarkovRunManager: public QObject
+{
+    Q_OBJECT
+public:
+    static MarkovRunManager* getInstance();
+
+private:
+    MarkovRunManager();
+    MarkovRunManager(MarkovRunManager const&);
+    void operator=(MarkovRunManager const&);
+
+    static MarkovRunManager* _instance;
+
+    //the alphabet and rules list.
+    //It can be changed while algorithm is running.
+    MarkovAlgorithm _algorithm;
+
+    //the input word of last run.
+    QString _input_word;
+
+    //the number of steps made.
+    int _steps_made;
+
+    //the word after last rule was applied
+    QString _word_after_last_step;
+
+    //break points.
+    QSet<int> _break_points;
+
+    //stores each step result to prevent program from cycle.
+    QSet<StepResult> _steps_history;
+
+    //whether the program now in debug mode.
+    bool _is_debug_mode;
+
+    //This function return true if the next should be made and false otherwise.
+    bool findAndApplyNextRule();
+
+    int getStepNumberOfValue(QString word);
+    bool choseAndUseRule(QString &word, MarkovRule &rule);
+
+
+public slots:
+    //called when new compiled algorithm is available.
+    void setAlgorithm(MarkovAlgorithm algorithm);
+
+    //true if the last version of source code is ready for running.
+    //When changed the canRunSourceCode should be emited.
+    void setCanRunSourceCode(bool can);
+
+    //starts running process.
+    void runWithoutDebug(QString input_word);
+
+    //starts running with debug.
+    void runWithDebug(QString input_word);
+
+    // add break point to set
+    void addBreakPoint(int line_number);
+
+    //removes break point from set
+    void removeBreakPoint(int line_number);
+
+    //called when user clicks Next Step in debug window.
+    void debugNextStep();
+
+    //called when user clicks Continue in debug window
+    void debugContinue();
+
+    //called when user clicks Stop in debug window
+    void debugStop();
+
+signals:
+    //emited when setCanRunSourceCode is called
+    void canRunSourceCode(bool);
+
+    void runWithoutDebugStarted(QString input_word);
+
+    void runWithoutDebugFinishSuccess(QString input_word,
+                                      QString output_word,
+                                      int steps_made);
+
+    void runWithoutDebugFinishFail(QString input_word,
+                                   RunError error,
+                                   int steps_made);
+
+    void runStepsMade(int steps_made);
+
+    void debugStarted(QString input_word);
+
+    void debugFinishSuccess(QString input_word,
+                            QString output_word,
+                            int steps_made);
+
+    void debugFinishFail(QString input_word,
+                         RunError error,
+                         int steps_made);
+
+    void debugStepFinished(int step_number,
+                           QString before_step,
+                           QString after_step,
+                           MarkovRule rule);
+
+    void debugBreakPointReached(int line_number);
+};
+
+
+
 
 #endif // MARKOVRUNMANAGER_H
