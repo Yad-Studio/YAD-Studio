3005c2566835e7239b7bb1801f774ab3a8f6131a
Diana Sirenko <dianasirenko@gmail.com>
2014-03-06 15:24:05 +0200

    close #18 add Source Code Manager

--- YAD-Studio/Managers/SourceCodeManager.cpp
+++ YAD-Studio/Managers/SourceCodeManager.cpp
@@ -1,5 +1,27 @@
 #include "SourceCodeManager.h"
 
-SourceCodeManager::SourceCodeManager()
+SourceCodeManager* SourceCodeManager::_instance = nullptr;
+SourceCodeManager::SourceCodeManager():
+    _source_code("")
 {
+}
+SourceCodeManager* SourceCodeManager::getInstance( )
+{
+      if (_instance == nullptr)
+        _instance = new SourceCodeManager();
+     return _instance;
+}
+QString SourceCodeManager::getSourceCode()
+{
+    return _source_code;
+}
+void SourceCodeManager::setSourceCode(QString new_source)
+{
+    _source_code = new_source;
+    emit sourceCodeChanged(new_source);
+}
+void SourceCodeManager::setNewSourceCodeFromFile(QString new_source)
+{
+    _source_code = new_source;
+    emit newSourceCodeWasLoaded(new_source);
 }
--- YAD-Studio/Managers/SourceCodeManager.h
+++ YAD-Studio/Managers/SourceCodeManager.h
@@ -1,10 +1,43 @@
 #ifndef SOURCECODEMANAGER_H
 #define SOURCECODEMANAGER_H
+#include "QString"
+#include <QObject>
 
-class SourceCodeManager
+class SourceCodeManager: public QObject
 {
+    Q_OBJECT
 public:
+    static SourceCodeManager* getInstance( );
+
+    //returns the value of _source_code
+    QString getSourceCode();
+
+private:
+    //the value which user inputs to the editor
+    QString _source_code;
+    static SourceCodeManager* _instance;
+
+    //it is a singletone
     SourceCodeManager();
+    SourceCodeManager(SourceCodeManager const&);
+    void operator=(SourceCodeManager const&);
+
+public slots:
+    //called when somebody changes the source code.
+    //new_source should be saved and sourceCodeChanged signal
+    //should be emitted.
+    void setSourceCode(QString new_source);
+
+    //called when new source code was loaded from file.
+    //The newSourceCodeWasLoaded should be emitted.
+    void setNewSourceCodeFromFile(QString new_source);
+
+signals:
+    //emitted when the source code changed.
+    void sourceCodeChanged(QString source_code);
+
+    //emitted when the source code was loaded from file.
+    void newSourceCodeWasLoaded(QString source_code);
 };
 
 #endif // SOURCECODEMANAGER_H
