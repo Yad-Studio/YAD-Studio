3a269281d7a22f1401d505cb6ce15e1df5537a0b
Diana Sirenko <dianasirenko@gmail.com>
2014-03-06 14:34:48 +0200

    #19 Input Widget add functions

--- YAD-Studio/Widgets/InputWidget.cpp
+++ YAD-Studio/Widgets/InputWidget.cpp
@@ -1,14 +1,71 @@
 #include "InputWidget.h"
 #include "ui_InputWidget.h"
+#include "QPushButton"
 
 InputWidget::InputWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::InputWidget)
 {
     ui->setupUi(this);
+
+    _can_run_algorythm = true;
+
+    //run button clicked
+    connect(ui->runButton,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(runCliked(bool)));
+
+    //run with debug cliked
+    connect(ui->runWithDebug,
+            SIGNAL(clicked()),
+            this,
+            SLOT(runWithDebugClicked()));
 }
 
 InputWidget::~InputWidget()
 {
     delete ui;
+}
+void InputWidget::runStarted()
+{
+    ui->runButton->setEnabled(false);
+    ui->runWithDebug->setEnabled(false);
+}
+
+void InputWidget::runFinished()
+{
+    if(_can_run_algorythm)
+    {
+        ui->runButton->setEnabled(true);
+        ui->runWithDebug->setEnabled(true);
+    }
+    else
+    {
+        ui->runButton->setEnabled(false);
+        ui->runWithDebug->setEnabled(false);
+    }
+}
+
+void InputWidget::setInput(QString new_input_word)
+{
+    ui->lineEdit->setText(new_input_word);
+}
+
+void InputWidget::canRunAlgorithm(bool can_run)
+{
+    _can_run_algorythm = can_run;
+}
+void InputWidget::runCliked(bool )
+{
+    int i=0;
+    ++i;
+    i+=2;
+    emit run(ui->lineEdit->text());
+    emit addToHistory(ui->lineEdit->text());
+}
+void InputWidget::runWithDebugClicked()
+{
+    emit runWithDebug(ui->lineEdit->text());
+    emit addToHistory(ui->lineEdit->text());
 }
--- YAD-Studio/Widgets/InputWidget.h
+++ YAD-Studio/Widgets/InputWidget.h
@@ -14,9 +14,48 @@ class InputWidget : public QWidget
 public:
     explicit InputWidget(QWidget *parent = 0);
     ~InputWidget();
+
+public slots:
+
+    //is called when the algorithm is starting running,
+    //the button Run and Run with debug should be disabled
+    void runStarted();
+
+    //is called when the algorithm is finished running,
+    //the buttons Run and Run with debug MAY be enabled
+    void runFinished();
+
+    //the value of Input Word should be changed to new value.
+    void setInput(QString new_input_word);
+
+    // when true is received and runStarted has not been called then Run
+    //and Run with debug should become enabled.
+    //When false is received then Run and Run with debug
+    //should be disabled
+    void canRunAlgorithm(bool can_run);
+
+signals:
+    //should be emitted when user clicks run.
+    //The value of Input Word field should be passed as argument
+    void run(QString input_word);
+
+    //the same as previous but when the Run with debug button is clicked.
+    void runWithDebug(QString input_word);
+
+    //when button is clicked
+    void save();
+
+    //should be emitted when run or runWithDebug is emitted.
+    //Used to notify HistoryStorage about new word.
+    void addToHistory(QString input_word);
     
 private:
     Ui::InputWidget *ui;
+
+    bool _can_run_algorythm;
+
+    void runCliked(bool);
+    void runWithDebugClicked();
 };
 
 #endif // INPUTWIDGET_H
