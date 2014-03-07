ee19385ea37ef794b6c0d2312a35958b644f715c
Diana Sirenko <dianasirenko@gmail.com>
2014-03-07 11:30:28 +0200

    #27 add 2 function Run With Debug

--- YAD-Studio/Widgets/DebugRunWidget.cpp
+++ YAD-Studio/Widgets/DebugRunWidget.cpp
@@ -1,14 +1,79 @@
 #include "DebugRunWidget.h"
 #include "ui_DebugRunWidget.h"
 
+
 DebugRunWidget::DebugRunWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::DebugRunWidget)
 {
     ui->setupUi(this);
+
+    debugStarted("");
 }
 
 DebugRunWidget::~DebugRunWidget()
 {
     delete ui;
+}
+void DebugRunWidget::debugStarted(QString input_word)
+{
+    ui->inputWidget->setWord(input_word);
+    ui->outputWidget->close();
+
+    ui->error->setVisible(false);
+    ui->errorDescription->setVisible(false);
+
+    ui->rule->setVisible(false);
+    ui->ruleText->setVisible(false);
+
+    ui->steps->setVisible(false);
+    ui->stepsNumber->setVisible(false);
+
+    ui->stopButton->setVisible(false);
+    ui->continueButton->setVisible(false);
+    ui->nextButton->setVisible(false);
+}
+
+void DebugRunWidget::debugSuccess(QString input_word,
+             QString output_word,
+             int steps_made)
+{
+    ui->inputWidget->setWord(input_word);
+    ui->outputWidget->setWord(output_word);
+
+    ui->error->setVisible(false);
+    ui->errorDescription->setVisible(false);
+
+    ui->rule->setVisible(false);
+    ui->ruleText->setVisible(false);
+
+    ui->steps->setVisible(true);
+    ui->stepsNumber->setVisible(true);
+    ui->stepsNumber->setText(QString::number(steps_made));
+
+    ui->stopButton->setVisible(false);
+    ui->continueButton->setVisible(false);
+    ui->nextButton->setVisible(false);
+
+
+}
+
+void DebugRunWidget::debugFailed(QString input_word,
+            RunError error,
+            int steps_made)
+{
+
+}
+
+void DebugRunWidget::debugStepFinished(int step_number,
+                  QString before_rule_applied,
+                  QString after_rule_applied,
+                  MarkovRule applied_rule)
+{
+
+}
+
+void DebugRunWidget::breakPointReached(int line_number)
+{
+
 }
--- YAD-Studio/Widgets/DebugRunWidget.h
+++ YAD-Studio/Widgets/DebugRunWidget.h
@@ -2,6 +2,8 @@
 #define DEBUGRUNWIDGET_H
 
 #include <QWidget>
+#include "Logic/MarkovRule.h"
+#include "Logic/RunError.h"
 
 namespace Ui {
 class DebugRunWidget;
@@ -17,6 +19,64 @@ public:
     
 private:
     Ui::DebugRunWidget *ui;
+
+public slots:
+
+    //should clear all previously set data
+    //and prepare to new debug session.
+    //If the widget is hidden it should show itself.
+    void debugStarted(QString input_word);
+
+    //widget should go to state 1 and fill in received info.
+    void debugSuccess(QString input_word,
+                 QString output_word,
+                 int steps_made);
+
+    //widget should go to state 2 and display received info.
+    void debugFailed(QString input_word,
+                RunError error,
+                int steps_made);
+
+    // widget should go to state 4.
+    //Add info to Debug Log about this rule and fill in all information.
+    void debugStepFinished(int step_number,
+                      QString before_rule_applied,
+                      QString after_rule_applied,
+                      MarkovRule applied_rule);
+
+    //when this slot is called only after debugStepFinished.
+    //So the widget is in state 4 and should transform to state 3
+    //by showing buttons.
+    void breakPointReached(int line_number);
+
+signals:
+    //the signal should be triggered when Next Step button is clicked.
+    //When this signal is triggered then removeBreakPoint()
+    //also must be triggered.
+    //Note widget should not do any other activity and must wait
+    //until any slots will be called.
+    void nextStepClicked();
+
+    //the signal should be triggered when Continue button is clicked.
+    //When this signal is triggered then removeBreakPoint() also must be
+    //triggered. Note widget should not do any other activity and
+    //must wait until any slots will be called.
+    void continueClicked();
+
+    //the signal should be triggered when Stop button is clicked.
+    //When this signal is triggered then removeBreakPoint() also must be
+    //triggered. Note widget should not do any other activity and must
+    //wait until any slots will be called.
+    void stopClicked();
+
+    //this signal will notify the TextEdit to line with break point
+    //highlighting.
+    void removeBreakPoint();
+
+    //notifies the text edit that debug stopped at given number.
+    //This signal should be emitted when the breakPointReached slot
+    //is called.
+    void showBreakPoint(int line_number);
 };
 
 #endif // DEBUGRUNWIDGET_H
